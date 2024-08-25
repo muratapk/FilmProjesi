9ad2b877aac0f1593b5a45381dc2d96e77b14c04
muratapk <muratapk@hotmail.com>
2024-08-25 15:39:46 +0300

    tema atılması

---
+++ FilmProjesi/Components/FeaturedList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmProjesi.Components
+{
+    public class FeaturedList:ViewComponent
+    {
+        VideosManager vm = new VideosManager(new EfVideosRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var result = vm.GetAll();
+            return View(result);
+        }
+    }
+}

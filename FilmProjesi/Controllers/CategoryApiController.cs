c7061fd739e52759260563bb54e0ae7123118dff
muratapk <muratapk@hotmail.com>
2024-08-31 18:56:34 +0300

    Category api ekleme silme işlemleri

--- FilmProjesi/Controllers/CategoryApiController.cs
+++ FilmProjesi/Controllers/CategoryApiController.cs
@@ -1,6 +1,7 @@
 ﻿using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace FilmProjesi.Controllers
 {
@@ -27,6 +28,70 @@ namespace FilmProjesi.Controllers
             }
           
             return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        { 
+           return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            try
+            {
+               string data=JsonConvert.SerializeObject(category);
+               StringContent content=new StringContent(data,Encoding.UTF8,"Application/Json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/Category/",content).Result;
+                if(response.IsSuccessStatusCode) {
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["Error"] = ex;
+                return View();
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Category category = new Category();
+                HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Category/" + id).Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    category=JsonConvert.DeserializeObject<Category>(data);
+                    View(category);
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["Error"] = ex;
+                return View();
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(int id, Category category)
+        {
+            try
+            {
+                HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + "/Category/" + id).Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    TempData["Success"] = "İşlem Başarılı";
+                    return RedirectToAction("index");
+                }
+            }
+            catch(Exception ex)
+            {
+                TempData["Error"] = "Hata Oluştu";
+                return View();
+            }
+            return View();
         }
     }
 }

f2e1fcac935c3e7c6bdae7bf6ba8eb860b1516b7
muratapk <muratapk@hotmail.com>
2024-09-01 19:08:28 +0300

    Jwt token oluşturulması

---
+++ FilmApi/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FilmApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public string Get(string username, string password)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name,username),
+                new Claim(JwtRegisteredClaimNames.Name,password),
+            };
+            var singinKey = "Bubiraribilgiyazilimdir";
+            //biranahtar oluştur diyoruz.
+            var securityKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(singinKey));
+            //bu anahtarı simetrikgüvenlik anahtarı olarak oluşturu diyoruz.
+        
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var jwtSecurityToken = new JwtSecurityToken(
+                 issuer: "http://filmprojesi.com",
+                 audience: "Bubiraribilgiyazilimidir",
+                 claims: claims,
+                 expires: DateTime.Now.AddDays(15),
+                 notBefore:DateTime.Now,
+                 signingCredentials:credentials
+
+                ) ;
+            //yukarıda oluşturduğumuz token bilgilisi jwtSecurityToken yaz
+            var token= new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            return token;
+        }
+    }
+}
